09b0a9e8e3ad682b099699cd7f92bfa12db77006
Taro77777 <tarochamberlain123@gmail.com>
2026-03-23 17:42:44 +0000

    copying everything from scaffolding files to work from

--- app/app.js
+++ app/app.js
@@ -7,31 +7,101 @@ var app = express();
 // Add static files location
 app.use(express.static("static"));
 
+// Use the Pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
+// Get the student model
+const { Student } = require("./models/student");
+
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+    res.render("index");
 });
+
+
+// Task 1 JSON formatted listing of students
+app.get("/all-students", function(req, res) {
+    var sql = 'select * from Students';
+    // As we are not inside an async function we cannot use await
+    // So we use .then syntax to ensure that we wait until the 
+    // promise returned by the async function is resolved before we proceed
+    db.query(sql).then(results => {
+        console.log(results);
+        res.json(results);
+    });
+
+});
+
+// Task 2 display a formatted list of students
+app.get("/all-students-formatted", function(req, res) {
+    var sql = 'select * from Students';
+    db.query(sql).then(results => {
+    	    // Send the results rows to the all-students template
+    	    // The rows will be in a variable called data
+        res.render('all-students', {data: results});
+    });
+});
+
+// Single student page.  Show the students name, course and modules
+app.get("/student-single/:id", async function (req, res) {
+    var stId = req.params.id;
+    // Create a student class with the ID passed
+    var student = new Student(stId);
+    await student.getStudentName();
+    await student.getStudentProgramme();
+    await student.getStudentModules();
+    console.log(student);
+    res.render('student', {student:student});
+});
+
+
+
+// JSON output of all programmes
+app.get("/all-programmes", function(req, res) {
+    var sql = 'select * from Programmes';
+    // As we are not inside an async function we cannot use await
+    // So we use .then syntax to ensure that we wait until the 
+    // promise returned by the async function is resolved before we proceed
+    db.query(sql).then(results => {
+        console.log(results);
+        res.json(results);
+    });
+
+});
+
+// Single programme page (no formatting or template)
+app.get("/programme-single/:id", async function (req, res) {
+    var pCode = req.params.id;
+    var pSql = "SELECT * FROM Programmes WHERE id = ?";
+    var results = await db.query(pSql, [pCode]);
+    //Now call the database for the modules
+    //Why do you think that the word modules is coming in before the name of the programme??
+    var modSql = "SELECT * FROM Programme_Modules pm \
+    JOIN Modules m on m.code = pm.module \
+    WHERE programme = ?";
+    var modResults = await db.query(modSql, [pCode]);
+    // String the results together, just for now.  Later we will push this
+    // through the template
+    res.send(JSON.stringify(results) + JSON.stringify(modResults));  
+});
+
 
 // Create a route for testing the db
 app.get("/db_test", function(req, res) {
     // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
+    var sql = 'select * from test_table';
+    // As we are not inside an async function we cannot use await
+    // So we use .then syntax to ensure that we wait until the 
+    // promise returned by the async function is resolved before we proceed
     db.query(sql).then(results => {
         console.log(results);
-        res.send(results)
+        res.json(results)
     });
 });
-
-app.get("/dbtest2", function(req, res) {
-    sql = 'select * from Programmes';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
-})
 
 // Create a route for /goodbye
 // Responds to a 'GET' request
@@ -49,35 +119,6 @@ app.get("/hello/:name", function(req, res) {
     //  Retrieve the 'name' parameter and use it in a dynamically generated page
     res.send("Hello " + req.params.name);
 });
-
-app.get("/user/:id_number", function(req, res) {
-    console.log(req.params);
-    res.send("Hello " + req.params.id_number);
-});
-
-app.get("/student/:name/:id_number", function(req,res) {
-    console.log(req.params)
-    res.send("Hello " + req.params.name + " " + req.params.id_number)
-});
-
-
-
-app.get("/roehampton", function(req, res) {
-    res.send("Hello Roehampton!")
-    console.log(req.url)
-    let path = req.url;
-    res.send(path.substring(0,3))
-});
-
-// Create a route for root - /
-app.get("/", function(req, res) {
-    res.send("Hello world!")
-})
-
-
-app.get("/banana", function(req, res) {
-    res.send("banana!!!!!?!?!?!?")
-})
 
 // Start server on port 3000
 app.listen(3000,function(){
